943a05a167f28bd91934d9cf98e11a0d88cb56f5
Barry E. Moore II <bmooreii@Barrys-MacBook-Pro.local>
2016-10-21 22:23:07 -0400

    Change function to lamdba, use c++11 brace initialization

--- midpoint_approx_pi/main.cpp
+++ midpoint_approx_pi/main.cpp
@@ -1,28 +1,25 @@
-#include <boost/mpi/environment.hpp>
-#include <boost/mpi/communicator.hpp>
 #include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <boost/mpi/environment.hpp>
 #include <boost/format.hpp>
 #include <iostream>
 #include <math.h>
 namespace mpi = boost::mpi;
-
-double approx(const double &a){
-  return 4.0 / (1.0 + a * a);
-}
 
 int main()
 {
   mpi::environment env;
   mpi::communicator world;
 
-  const double exact_pi = 4.0 * atan(1.0);
-  const int num_grid = 1000000;
-  double approx_pi = 0, to_reduce_pi = 0, d_grid = 0, sum = 0, inner_temp = 0;
+  auto approx = [](double a){return 4.0 / (1.0 + a * a);};
+  const double exact_pi{4.0 * atan(1.0)};
+  const int num_grid{100000};
+  double approx_pi{0}, to_reduce_pi{0}, d_grid{0}, sum{0}, inner_temp{0};
 
   // Approximate pi over the grid
   d_grid = 1.0 / (double) num_grid;
   for (int i = world.rank(); i < num_grid; i += world.size()){
-    inner_temp = d_grid * ((double) i - 0.5);
+    inner_temp = d_grid * ((double) i + 0.5);
     sum += approx(inner_temp);
   }
   to_reduce_pi = d_grid * sum;
@@ -32,7 +29,8 @@ int main()
 
   // Print the approx_pi
   if (world.rank() == 0)
-    std::cout << boost::format("Pi is approximately %.16f, error is %.16f\n") % approx_pi % (exact_pi - approx_pi);
+    std::cout << boost::format("Pi is approximately %.16f, error is %.16f\n")
+      % approx_pi % (exact_pi - approx_pi);
 
   return 0;
 }
